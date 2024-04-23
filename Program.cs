864de9b01cfce102df58a5a46dd387cf561ceb67
Jacek Wronski <jacek.wronski@codiceplastico.com>
2024-04-23 09:00:33 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using var server = new Prometheus.MetricServer(port: 1234);
+server.Start();
+
+Console.WriteLine("Open http://localhost:1234/metrics in a web browser.");
+Console.WriteLine("Press enter to exit.");
+Console.ReadLine();
